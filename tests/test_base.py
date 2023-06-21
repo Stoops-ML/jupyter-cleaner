e526117a576a1b36d7a37b4d1c49a5a05ccd27d3
Daniel Stoops <danielst@omnisys.co.il>
2023-06-21 19:51:22 +0300

    initial commit

---
+++ tests/test_base.py
@@ -0,0 +1,507 @@
+import json
+import os
+import tempfile
+from pathlib import Path
+
+from jupyter_cleaner.jupyter_cleaner import get_lab_files  # type: ignore
+from jupyter_cleaner.jupyter_cleaner import parse_pyproject  # type: ignore
+from jupyter_cleaner.jupyter_cleaner import process_inputs  # type: ignore
+from jupyter_cleaner.jupyter_cleaner import run  # type: ignore
+
+
+def test_defaults() -> None:
+    """Test run() with defaults"""
+    data = {
+        "cells": [
+            {
+                "cell_type": "code",
+                "execution_count": 1,
+                "metadata": {},
+                "outputs": [
+                    {
+                        "data": {
+                            "text/html": [
+                                "\n",
+                                '                <script type="application/javascript" id="jupyter_reorder_python_imports">\n',
+                                "                (function() {\n",
+                                "                    if (window.IPython === undefined) {\n",
+                                "                        return\n",
+                                "                    }\n",
+                                '                    var msg = "WARNING: it looks like you might have loaded " +\n',
+                                '                        "jupyter_reorder_python_imports in a non-lab notebook with " +\n',
+                                '                        "`is_lab=True`. Please double check, and if " +\n',
+                                '                        "loading with `%load_ext` please review the README!"\n',
+                                "                    console.log(msg)\n",
+                                "                    alert(msg)\n",
+                                "                })()\n",
+                                "                </script>\n",
+                                "                ",
+                            ],
+                            "text/plain": ["<IPython.core.display.HTML object>"],
+                        },
+                        "metadata": {},
+                        "output_type": "display_data",
+                    }
+                ],
+                "source": [
+                    "%load_ext jupyter_reorder_python_imports\n",
+                    "%load_ext jupyter_black",
+                ],
+            },
+            {
+                "cell_type": "code",
+                "execution_count": 5,
+                "metadata": {},
+                "outputs": [],
+                "source": ["import re\n", "import datetime"],
+            },
+            {
+                "cell_type": "code",
+                "execution_count": 10,
+                "metadata": {},
+                "outputs": [
+                    {
+                        "data": {"text/plain": ["1"]},
+                        "execution_count": 4,
+                        "metadata": {},
+                        "output_type": "execute_result",
+                    }
+                ],
+                "source": ["a=1\na"],
+            },
+        ],
+        "metadata": {
+            "kernelspec": {
+                "display_name": "jreorder-1i-52Iue",
+                "language": "python",
+                "name": "python3",
+            },
+            "language_info": {
+                "codemirror_mode": {"name": "ipython", "version": 3},
+                "file_extension": ".py",
+                "mimetype": "text/x-python",
+                "name": "python",
+                "nbconvert_exporter": "python",
+                "pygments_lexer": "ipython3",
+                "version": "3.10.10",
+            },
+            "orig_nbformat": 4,
+        },
+        "nbformat": 4,
+        "nbformat_minor": 2,
+    }
+    expected_result = {
+        "cells": [
+            {
+                "cell_type": "code",
+                "execution_count": 0,
+                "metadata": {},
+                "outputs": [],
+                "source": [
+                    "%load_ext jupyter_reorder_python_imports\n",
+                    "%load_ext jupyter_black",
+                ],
+            },
+            {
+                "cell_type": "code",
+                "execution_count": 0,
+                "metadata": {},
+                "outputs": [],
+                "source": ["import datetime\n", "import re"],
+            },
+            {
+                "cell_type": "code",
+                "execution_count": 0,
+                "metadata": {},
+                "outputs": [],
+                "source": ["a = 1\n", "a"],
+            },
+        ],
+        "metadata": {
+            "kernelspec": {
+                "display_name": "jreorder-1i-52Iue",
+                "language": "python",
+                "name": "python3",
+            },
+            "language_info": {
+                "codemirror_mode": {"name": "ipython", "version": 3},
+                "file_extension": ".py",
+                "mimetype": "text/x-python",
+                "name": "python",
+                "nbconvert_exporter": "python",
+                "pygments_lexer": "ipython3",
+                "version": "3.10.10",
+            },
+            "orig_nbformat": 4,
+        },
+        "nbformat": 4,
+        "nbformat_minor": 2,
+    }
+
+    file = tempfile.NamedTemporaryFile(suffix=".ipynb", delete=False)
+    with open(file.name, "w") as f:
+        json.dump(data, f)
+    run([Path(file.name)])
+    with open(file.name) as f:
+        formatted_data = json.load(f)
+    assert formatted_data == expected_result
+
+
+def test_inputs() -> None:
+    """Test run() with defined inputs"""
+    data = {
+        "cells": [
+            {
+                "cell_type": "code",
+                "execution_count": 1,
+                "metadata": {},
+                "outputs": [
+                    {
+                        "data": {
+                            "text/html": [
+                                "\n",
+                                '                <script type="application/javascript" id="jupyter_reorder_python_imports">\n',
+                                "                (function() {\n",
+                                "                    if (window.IPython === undefined) {\n",
+                                "                        return\n",
+                                "                    }\n",
+                                '                    var msg = "WARNING: it looks like you might have loaded " +\n',
+                                '                        "jupyter_reorder_python_imports in a non-lab notebook with " +\n',
+                                '                        "`is_lab=True`. Please double check, and if " +\n',
+                                '                        "loading with `%load_ext` please review the README!"\n',
+                                "                    console.log(msg)\n",
+                                "                    alert(msg)\n",
+                                "                })()\n",
+                                "                </script>\n",
+                                "                ",
+                            ],
+                            "text/plain": ["<IPython.core.display.HTML object>"],
+                        },
+                        "metadata": {},
+                        "output_type": "display_data",
+                    }
+                ],
+                "source": [
+                    "%load_ext jupyter_reorder_python_imports\n",
+                    "%load_ext jupyter_black",
+                ],
+            },
+            {
+                "cell_type": "code",
+                "execution_count": 5,
+                "metadata": {},
+                "outputs": [],
+                "source": ["import re\n", "import datetime"],
+            },
+            {
+                "cell_type": "code",
+                "execution_count": 10,
+                "metadata": {},
+                "outputs": [
+                    {
+                        "data": {"text/plain": ["1"]},
+                        "execution_count": 4,
+                        "metadata": {},
+                        "output_type": "execute_result",
+                    }
+                ],
+                "source": ["a=1\na"],
+            },
+        ],
+        "metadata": {
+            "kernelspec": {
+                "display_name": "jreorder-1i-52Iue",
+                "language": "python",
+                "name": "python3",
+            },
+            "language_info": {
+                "codemirror_mode": {"name": "ipython", "version": 3},
+                "file_extension": ".py",
+                "mimetype": "text/x-python",
+                "name": "python",
+                "nbconvert_exporter": "python",
+                "pygments_lexer": "ipython3",
+                "version": "3.10.10",
+            },
+            "orig_nbformat": 4,
+        },
+        "nbformat": 4,
+        "nbformat_minor": 2,
+    }
+
+    expected_result = {
+        "cells": [
+            {
+                "cell_type": "code",
+                "execution_count": 100,
+                "metadata": {},
+                "outputs": [
+                    {
+                        "data": {
+                            "text/html": [
+                                "\n",
+                                '                <script type="application/javascript" id="jupyter_reorder_python_imports">\n',
+                                "                (function() {\n",
+                                "                    if (window.IPython === undefined) {\n",
+                                "                        return\n",
+                                "                    }\n",
+                                '                    var msg = "WARNING: it looks like you might have loaded " +\n',
+                                '                        "jupyter_reorder_python_imports in a non-lab notebook with " +\n',
+                                '                        "`is_lab=True`. Please double check, and if " +\n',
+                                '                        "loading with `%load_ext` please review the README!"\n',
+                                "                    console.log(msg)\n",
+                                "                    alert(msg)\n",
+                                "                })()\n",
+                                "                </script>\n",
+                                "                ",
+                            ],
+                            "text/plain": ["<IPython.core.display.HTML object>"],
+                        },
+                        "metadata": {},
+                        "output_type": "display_data",
+                    }
+                ],
+                "source": [
+                    "%load_ext jupyter_reorder_python_imports\n",
+                    "%load_ext jupyter_black",
+                ],
+            },
+            {
+                "cell_type": "code",
+                "execution_count": 100,
+                "metadata": {},
+                "outputs": [],
+                "source": ["import re\n", "import datetime"],
+            },
+            {
+                "cell_type": "code",
+                "execution_count": 100,
+                "metadata": {},
+                "outputs": [
+                    {
+                        "data": {"text/plain": ["1"]},
+                        "execution_count": 4,
+                        "metadata": {},
+                        "output_type": "execute_result",
+                    }
+                ],
+                "source": ["a=1\na"],
+            },
+        ],
+        "metadata": {
+            "kernelspec": {
+                "display_name": "jreorder-1i-52Iue",
+                "language": "python",
+                "name": "python3",
+            },
+            "language_info": {
+                "codemirror_mode": {"name": "ipython", "version": 3},
+                "file_extension": ".py",
+                "mimetype": "text/x-python",
+                "name": "python",
+                "nbconvert_exporter": "python",
+                "pygments_lexer": "ipython3",
+                "version": "3.10.10",
+            },
+            "orig_nbformat": 4,
+        },
+        "nbformat": 4,
+        "nbformat_minor": 2,
+    }
+
+    file = tempfile.NamedTemporaryFile(suffix=".ipynb", delete=False)
+    with open(file.name, "w") as f:
+        json.dump(data, f)
+    run(
+        [Path(file.name)],
+        execution_count=100,
+        remove_code_output=False,
+        format=False,
+        reorder_imports=False,
+    )
+    with open(file.name) as f:
+        formatted_data = json.load(f)
+    assert formatted_data == expected_result
+
+
+def test_pyroject() -> None:
+    """Test run() with pyproject file"""
+
+    data = {
+        "cells": [
+            {
+                "cell_type": "code",
+                "execution_count": 1,
+                "metadata": {},
+                "outputs": [
+                    {
+                        "data": {
+                            "text/html": [
+                                "\n",
+                                '                <script type="application/javascript" id="jupyter_reorder_python_imports">\n',
+                                "                (function() {\n",
+                                "                    if (window.IPython === undefined) {\n",
+                                "                        return\n",
+                                "                    }\n",
+                                '                    var msg = "WARNING: it looks like you might have loaded " +\n',
+                                '                        "jupyter_reorder_python_imports in a non-lab notebook with " +\n',
+                                '                        "`is_lab=True`. Please double check, and if " +\n',
+                                '                        "loading with `%load_ext` please review the README!"\n',
+                                "                    console.log(msg)\n",
+                                "                    alert(msg)\n",
+                                "                })()\n",
+                                "                </script>\n",
+                                "                ",
+                            ],
+                            "text/plain": ["<IPython.core.display.HTML object>"],
+                        },
+                        "metadata": {},
+                        "output_type": "display_data",
+                    }
+                ],
+                "source": [
+                    "%load_ext jupyter_reorder_python_imports\n",
+                    "%load_ext jupyter_black",
+                ],
+            },
+            {
+                "cell_type": "code",
+                "execution_count": 5,
+                "metadata": {},
+                "outputs": [],
+                "source": ["import re\n", "import datetime"],
+            },
+            {
+                "cell_type": "code",
+                "execution_count": 10,
+                "metadata": {},
+                "outputs": [
+                    {
+                        "data": {"text/plain": ["1"]},
+                        "execution_count": 4,
+                        "metadata": {},
+                        "output_type": "execute_result",
+                    }
+                ],
+                "source": ["a=1\na"],
+            },
+        ],
+        "metadata": {
+            "kernelspec": {
+                "display_name": "jreorder-1i-52Iue",
+                "language": "python",
+                "name": "python3",
+            },
+            "language_info": {
+                "codemirror_mode": {"name": "ipython", "version": 3},
+                "file_extension": ".py",
+                "mimetype": "text/x-python",
+                "name": "python",
+                "nbconvert_exporter": "python",
+                "pygments_lexer": "ipython3",
+                "version": "3.10.10",
+            },
+            "orig_nbformat": 4,
+        },
+        "nbformat": 4,
+        "nbformat_minor": 2,
+    }
+
+    expected_result = {
+        "cells": [
+            {
+                "cell_type": "code",
+                "execution_count": 0,
+                "metadata": {},
+                "outputs": [],
+                "source": [
+                    "%load_ext jupyter_reorder_python_imports\n",
+                    "%load_ext jupyter_black",
+                ],
+            },
+            {
+                "cell_type": "code",
+                "execution_count": 0,
+                "metadata": {},
+                "outputs": [],
+                "source": ["import datetime\n", "import re"],
+            },
+            {
+                "cell_type": "code",
+                "execution_count": 0,
+                "metadata": {},
+                "outputs": [],
+                "source": ["a = 1\n", "a"],
+            },
+        ],
+        "metadata": {
+            "kernelspec": {
+                "display_name": "jreorder-1i-52Iue",
+                "language": "python",
+                "name": "python3",
+            },
+            "language_info": {
+                "codemirror_mode": {"name": "ipython", "version": 3},
+                "file_extension": ".py",
+                "mimetype": "text/x-python",
+                "name": "python",
+                "nbconvert_exporter": "python",
+                "pygments_lexer": "ipython3",
+                "version": "3.10.10",
+            },
+            "orig_nbformat": 4,
+        },
+        "nbformat": 4,
+        "nbformat_minor": 2,
+    }
+
+    nb_file = Path("test.ipynb")
+    with open(nb_file, "w") as f:
+        json.dump(data, f)
+
+    args_execution_count = 1000
+    args_remove_code_output = False
+    args_format = False
+    args_reorder_imports = False
+    args_files_or_dirs = ["."]
+
+    (
+        project_files_or_dirs,
+        project_execution_count,
+        project_remove_code_output,
+        project_format,
+        project_reorder_imports,
+    ) = parse_pyproject()
+
+    (
+        files_or_dirs,
+        execution_count,
+        remove_code_output,
+        format,
+        reorder_imports,
+    ) = process_inputs(
+        args_files_or_dirs,
+        args_execution_count,
+        args_remove_code_output,
+        args_format,
+        args_reorder_imports,
+        project_files_or_dirs,
+        project_execution_count,
+        project_remove_code_output,
+        project_format,
+        project_reorder_imports,
+    )
+
+    files = get_lab_files(files_or_dirs)
+
+    run(
+        files,
+        execution_count,
+        remove_code_output,
+        format,
+        reorder_imports,
+    )
+    with open(nb_file) as f:
+        formatted_data = json.load(f)
+    assert formatted_data == expected_result
+    os.unlink(nb_file)
